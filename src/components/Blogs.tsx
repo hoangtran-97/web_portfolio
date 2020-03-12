4477dab26814a9be39946fa497f997365eef9564
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-12 22:18:52 +0200

    feat: added blog section

---
+++ src/components/Blogs.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import colors from "../constants/colors";
+
+export const Blogs = () => {
+    return (
+        <>
+            <h2 style={styles.header}>Blog Entries</h2>
+            <p>Content Comming Soon!</p>
+        </>
+    );
+};
+const styles = {
+    header: {color: colors.highlight, textAlign: "center" as "center"}
+};
