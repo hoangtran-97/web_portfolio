4ac3f52d75505bf72c364a0454f6d3a09dad750d
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-13 21:36:48 +0200

    fix: blog

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import colors from "../constants/colors";
-
+import {Link} from "react-router-dom";
+import {FiArrowLeft} from "react-icons/fi";
 export const Blogs = () => {
     return (
         <>
+            <Link to="/" className="nav-item" style={styles.link}>
+                <FiArrowLeft style={styles.icon}></FiArrowLeft> Back
+            </Link>
             <h2 style={styles.header}>Blog Entries</h2>
-            <p style={styles.content}>Content Comming Soon!</p>
+            <p style={styles.header}>Content Comming Soon!</p>
         </>
     );
 };
 const styles = {
     header: {color: colors.highlight, textAlign: "center" as "center"},
-    content: {color: colors.highlight}
+    content: {color: colors.highlight},
+    link: {
+        fontSize: 20,
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        color: colors.highlight,
+        textDecoration: "none"
+    },
+    icon: {
+        width: 20,
+        height: 20
+    }
 };
