460dfbdc67cfef24767e48deac10a8d719fd484e
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-13 12:51:44 +0200

    feat: hamburger menu working

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,7 +7,7 @@ import {NavigationPaths} from "./NavigationPaths";
 const Menu = () => {
     return (
         <Router>
-            <div style={styles.container}>
+            <div style={styles.menuContainer}>
                 <NavigationPaths></NavigationPaths>
             </div>
         </Router>
@@ -16,7 +16,7 @@ const Menu = () => {
 const MobileMenu = () => {
     return (
         <Router>
-            <div style={styles.container}>
+            <div style={styles.mobileMenuContainer}>
                 <NavigationPaths></NavigationPaths>
             </div>
         </Router>
@@ -26,24 +26,23 @@ export const Navigation = () => {
     const [open, setToggle] = useState(false);
     const MobileMenuIcon = () => {
         return (
-            <FiMenu
-                onClick={() => {
-                    setToggle(!open);
-                    console.log(open);
-                }}
-                style={styles.icon}
-            ></FiMenu>
+            <div>
+                <FiMenu
+                    onClick={() => {
+                        setToggle(!open);
+                        setTimeout(() => setToggle(false), 3000);
+                        console.log(open);
+                    }}
+                    style={styles.hamburgerIcon}
+                ></FiMenu>
+                {open ? <MobileMenu></MobileMenu> : null}
+            </div>
         );
     };
-    return (
-        <>
-            {window.innerWidth > 900 ? <Menu></Menu> : <MobileMenuIcon></MobileMenuIcon>}
-            {open ? <MobileMenu></MobileMenu> : null}
-        </>
-    );
+    return <>{window.innerWidth > 900 ? <Menu></Menu> : <MobileMenuIcon></MobileMenuIcon>}</>;
 };
 const styles = {
-    container: {
+    menuContainer: {
         fontSize: 20,
         fontWeight: 800,
         color: colors.highlight,
@@ -55,12 +54,41 @@ const styles = {
         boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
         height: "100%"
     },
+    mobileMenuContainer: {
+        fontSize: 20,
+        fontWeight: 800,
+        color: colors.highlight,
+        display: "flex",
+        flexDirection: "column" as "column",
+        justifyContent: "space-around",
+        alignItems: "flex-start",
+        borderRadius: "5px",
+        boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+        position: "fixed" as "fixed",
+        height: "200px",
+        width: "70vw",
+        backgroundColor: "rgba(68, 62, 62, 0.7)",
+        zIndex: 9999,
+        paddingLeft: "20px"
+    },
     icon: {
         color: colors.highlight,
         height: 25,
         width: 25
     },
+    hamburgerIcon: {
+        color: colors.highlight,
+        height: 25,
+        width: 25,
+        position: "fixed" as "fixed",
+        top: "20px",
+        left: "10px"
+    },
     menu: {
         color: colors.highlight
+    },
+    test: {
+        backgroundColor: colors.highlight,
+        position: "fixed" as "fixed"
     }
 };
