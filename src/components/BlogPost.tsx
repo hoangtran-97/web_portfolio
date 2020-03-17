c1ae2ec2e998076a28b6210c07cde6de91485b5d
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-17 11:46:41 +0200

    feature: block responsive design done

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import colors from "../constants/colors";
 import {animated, useSpring} from "react-spring";
+import {FiX} from "react-icons/fi";
 interface BlogPostProps {
     children: JSX.Element[] | JSX.Element;
     dateTime: string;
@@ -9,7 +10,7 @@ interface BlogPostProps {
 export const BlogPost = ({children, dateTime, title}: BlogPostProps) => {
     const [open, setToggle] = useState(false);
     const animation = useSpring({
-        width: open ? "600px" : "300px",
+        width: open ? "900px" : "300px",
         boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
         backgroundColor: colors.gray,
         color: colors.highlight,
@@ -22,14 +23,36 @@ export const BlogPost = ({children, dateTime, title}: BlogPostProps) => {
         }
     });
     const childrenAnimation = useSpring({
-        width: open ? "300px" : "0px",
+        width: open ? "600px" : "0px",
+        height: "300px",
+        padding: open ? "50px" : "0px",
+        overflow: "scroll",
         opacity: open ? 1 : 0,
         config: {
             duration: open ? 400 : 200
         }
     });
+    const titleAnimation = useSpring({
+        color: colors.highlight,
+        fontSize: 22,
+        fontWeight: "bold",
+        marginLeft: open ? "50px" : "0px",
+        backgroundImage: "url(https://source.unsplash.com/900x400/?linkedin)",
+        backgroundColor: "rgba(68, 62, 62, 0.7)",
+        backgroundBlendMode: "color",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: open ? "200px" : "300px",
+        height: open ? "200px" : "300px",
+        borderRadius: "5px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    });
     const mobileAnimation = useSpring({
-        height: open ? "600px" : "300px",
+        height: open ? "900px" : "300px",
+        width: "300px",
         boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
         backgroundColor: colors.gray,
         color: colors.highlight,
@@ -43,7 +66,9 @@ export const BlogPost = ({children, dateTime, title}: BlogPostProps) => {
         }
     });
     const mobileChildrenAnimation = useSpring({
-        height: open ? "300px" : "0px",
+        padding: open ? "20px" : "0px",
+        height: open ? "600px" : "0px",
+        overflow: "scroll",
         opacity: open ? 1 : 0,
         config: {
             duration: open ? 400 : 200
@@ -62,7 +87,16 @@ export const BlogPost = ({children, dateTime, title}: BlogPostProps) => {
                     <br />
                     {title}
                 </h3>
-                <animated.div style={mobileChildrenAnimation}>{children}</animated.div>
+                <animated.div style={mobileChildrenAnimation}>
+                    {children}
+                    <br />
+                    <FiX
+                        onClick={() => {
+                            setToggle(false);
+                        }}
+                        style={styles.closeIcon}
+                    ></FiX>
+                </animated.div>
             </animated.div>
         );
     };
@@ -74,11 +108,11 @@ export const BlogPost = ({children, dateTime, title}: BlogPostProps) => {
                     setToggle(!open);
                 }}
             >
-                <h3 style={styles.title}>
+                <animated.div style={titleAnimation}>
                     {dateTime}
                     <br />
                     {title}
-                </h3>
+                </animated.div>
                 <animated.div style={childrenAnimation}>{children}</animated.div>
             </animated.div>
         );
@@ -86,15 +120,6 @@ export const BlogPost = ({children, dateTime, title}: BlogPostProps) => {
     return <>{window.innerWidth > 900 ? <Title></Title> : <MobileTitle></MobileTitle>}</>;
 };
 const styles = {
-    container: {
-        boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
-        backgroundColor: colors.gray,
-        color: colors.highlight,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        borderRadius: "5px"
-    },
     title: {
         color: colors.highlight,
         margin: 0,
@@ -110,5 +135,13 @@ const styles = {
         display: "flex",
         justifyContent: "center",
         alignItems: "center"
+    },
+    closeIcon: {
+        margin: "10px",
+        width: 30,
+        height: 30,
+        color: colors.gray,
+        backgroundColor: colors.highlight,
+        borderRadius: "50px"
     }
 };
--- src/stories/Blog.stories.js
+++ src/stories/Blog.stories.js
@@ -10,9 +10,34 @@ export const showCode = () => {
     const code = text("code", "const testComponent = 5");
     return <CodeBlock language={language}>{code}</CodeBlock>;
 };
+const randomText = `Considered discovered ye sentiments projecting entreaties of melancholy is.
+ In expression an solicitude principles in do. Hard do me sigh with west same lady. 
+Their saved linen downs tears son add music. 
+Expression alteration entreaties mrs can terminated estimating. Her too add narrow having wished. 
+To things so denied admire. Am wound worth water he linen at vexed. 
+
+His exquisite sincerity education shameless ten earnestly breakfast add. 
+So we me unknown as improve hastily sitting forming. 
+Especially favourable compliment but thoroughly unreserved saw she themselves.
+ Sufficient impossible him may ten insensible put continuing.
+ Oppose exeter income simple few joy cousin but twenty. Scale began quiet up short wrong in in. Sportsmen shy forfeited engrossed may can. 
+
+Sportsman delighted improving dashwoods gay instantly happiness six. 
+Ham now amounted absolute not mistaken way pleasant whatever. 
+At an these still no dried folly stood thing. Rapid it on hours hills it seven years.
+ If polite he active county in spirit an. Mrs ham intention promotion engrossed assurance defective.
+ Confined so graceful building opinions whatever trifling in. 
+Insisted out differed ham man endeavor expenses. At on he total their he songs. Related compact effects is on settled do. 
+
+Consulted perpetual of pronounce me delivered. 
+Too months nay end change relied who beauty wishes matter.
+ Shew of john real park so rest we on. 
+Ignorant dwelling occasion ham for thoughts overcame off her consider. Polite it elinor is depend. 
+His not get talked effect worthy barton. Household shameless incommode at no objection behaviour. 
+Especially do at he possession insensible sympathize boisterous it. Songs he on an widen me event truth. Certain law age brother sending amongst why covered. `;
 export const showBlogPost = () => {
     const dateTime = text("dateTime", "Jun. 3");
-    const blogContent = text("blogContent", "I need a jub");
+    const blogContent = text("blogContent", randomText);
     const title = text("title", "Plz help");
     return (
         <BlogPost dateTime={dateTime} title={title}>
