686d430c5ff9172d0929aea49772b82301acb99c
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-17 09:28:35 +0200

    feat: working animation

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import colors from "../constants/colors";
+import {animated, useSpring} from "react-spring";
 interface BlogPostProps {
     children: JSX.Element[] | JSX.Element;
     dateTime: string;
     title: string;
 }
 export const BlogPost = ({children, dateTime, title}: BlogPostProps) => {
+    const [open, setToggle] = useState(false);
+    const animation = useSpring({
+        width: open ? "600px" : "300px",
+        boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+        backgroundColor: colors.gray,
+        color: colors.highlight,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        borderRadius: "5px",
+        config: {
+            duration: open ? 1000 : 200
+        }
+    });
+    const childrenAnimation = useSpring({
+        width: open ? "300px" : "0px",
+        opacity: open ? 1 : 0,
+        config: {
+            duration: open ? 1000 : 200
+        }
+    });
     return (
-        <div style={styles.container}>
+        <animated.div
+            style={animation}
+            onClick={() => {
+                setToggle(!open);
+            }}
+        >
             <h3 style={styles.title}>
                 {dateTime}
                 <br />
                 {title}
             </h3>
-            {children}
-        </div>
+            <animated.div style={childrenAnimation}>{children}</animated.div>
+        </animated.div>
     );
 };
 const styles = {
@@ -41,7 +68,6 @@ const styles = {
         borderRadius: "5px",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
-        marginRight: "10px"
+        alignItems: "center"
     }
 };
