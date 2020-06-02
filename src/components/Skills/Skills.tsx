5a8b6a22fe2cbe21442389e28b55396a41128d22
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-06-02 16:17:32 +0300

    reafactor: skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import "./style.css";
 
+const skills = ["React", "React Native", "JavaScript", "TypeScript", "Redux", "REST API", "Webpack", "Jest", "SASS", "MongoDB", "Node", "Express"];
+
+interface SkillProps {
+    skill: string;
+}
+const Skill = ({skill}: SkillProps) => <div className="skill_container">{skill}</div>;
 export const Skills = () => {
-    return <div id="skills">React, React Native, JavaScript, TypeScript, Redux, REST API, Webpack, Jest, SASS, MongoDB, Node, Express.</div>;
+    return (
+        <div className="skills_container">
+            //I Know this is an anti-pattern but in this case since the data is static it is safe
+            {skills.map((skill, index) => (
+                <Skill skill={skill} key={index} />
+            ))}
+        </div>
+    );
 };
